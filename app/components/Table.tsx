43dc628f638a30d9de0bf738c6fba275a07ff066
Alexey Tomin <alextomin@list.ru>
2023-05-24 10:41:08 +0200

    Suspense test

---
+++ app/components/Table.tsx
@@ -0,0 +1,35 @@
+// we define two variables, one holding data, and one holding a value if the promise
+//was resolved
+let myData = undefined;
+let promise = undefined;
+// our React component which will be rendered inside Suspense boundaries
+export default function Table() {
+  //our method which will be fired when we call the component
+  const waitForData = () => {
+    //if data already there, we return it and render is done, if not the Spinner is spinning
+    if (myData) return myData;
+    // an array to remember already computed values
+    if (!promise)
+      promise = fetch(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      )
+        .then((res) => res.json())
+        .then((d) => {
+          setTimeout(() => {
+            myData = d;
+          }, 10000)
+        });
+    // we can also throw a new Promise with rejection like below
+    // throw new Promise((undefined,rej)=>rej())
+    throw promise;
+  };
+
+  const data = waitForData();
+  // we fill in what is returned from the typicode API, if no data returned we display
+  // a fixed message
+  return (
+    <div className="w-full flex justify-center items-center h-screen">
+      {data.title || "Hello Ali"}
+    </div>
+  );
+};
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,31 @@
+import dynamic from 'next/dynamic';
 import Image from 'next/image'
+import Table from '@components/Table'
+import { Suspense } from 'react'
+
+const SuspenseTable = dynamic(
+  () => import("@components/Table"),
+  {
+    ssr: true, loading: () =>
+      <div className="w-full flex justify-center items-center h-screen">
+        ...loading
+      </div>
+  }
+);
 
 export default function Test() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       test page
-  
+
+      <Suspense fallback={
+        <div className="w-full flex justify-center items-center h-screen">
+          ...loading
+        </div>
+      }>
+        <SuspenseTable />
+      </Suspense>
+
     </main>
   )
 }
